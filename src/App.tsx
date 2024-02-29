e7bffc45dd0d8b97b0509c558beb4860131cb55c
gaga <gagasystemarchitect@gmail.com>
2024-02-29 11:12:46 +0800

    calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Calendar from "./Calendar";
+import dayjs from "dayjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div className="App">
+    {/* <Calendar value={dayjs('2023-11-08')} dateInnerContent={(value) => {
+      return <div>
+        <p style={{ background: 'yellowgreen', height: '300px' }}>{value.format("YYYY/MM/DD")}</p>
+      </div>
+    }} /> */}
+    <Calendar value={dayjs('2023-11-08')} locale="zh-CN" onChange={(date) => {
+      alert(date.format('YYYY-MM-DD'))
+    }} />
+  </div>
 }
-
-export default App;
